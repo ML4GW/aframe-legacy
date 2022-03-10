acc10234e66cc77fa8eb11391c203c76cf2d109b
ethan.marx <ethan.marx@ligo.org>
2022-03-10 13:05:13 -0800

    add ability to apply vetoes

--- projects/generate-glitch-dataset/generate_glitches.py
+++ projects/generate-glitch-dataset/generate_glitches.py
@@ -1,8 +1,26 @@
-import numpy as np
 import glob
 import h5py
 import os
+
+import numpy as np
+
 from gwpy.timeseries import TimeSeries
+from gwpy.segments import Segment, SegmentList
+
+import utils as utils
+
+
+'''
+Tools to generate a dataset of glitches from omicron triggers.
+
+For information on how the omicron triggers were generated see: /home/ethan.marx/bbhnet/generate-glitch-dataset/omicron/12566/H1L1_1256665618_100000/runfiles/omicron_params_H1.txt on CIT cluster
+for an example omicron parameter file. 
+
+Of note is the clustering timescale of 1 second
+
+'''
+
+
 
 
 def generate_glitch_dataset(
@@ -13,6 +31,7 @@ def generate_glitch_dataset(
     window: float,
     sample_rate: float = 4096,
     omicron_dir: str ='/home/ethan.marx/bbhnet/generate-glitch-dataset/omicron/',
+    vetoes: SegmentList = None,
 ):
 
     """
@@ -37,7 +56,6 @@ def generate_glitch_dataset(
     gps_day_start = str(start)[:5]
     gps_day_end = str(stop)[:5] 
     all_gps_days = np.arange(int(gps_day_start), int(gps_day_end) + 1, 1) 
-    print(all_gps_days)    
 
     # loop over gps days
     for i, day in enumerate(all_gps_days):
@@ -46,13 +64,17 @@ def generate_glitch_dataset(
         omicron_path = omicron_dir + f'{day}/'
         
         # do we want pre or post vetoes? I presume we want to use some glitches found in vetoes (CAT3, not CAT1)
-        print(glob.glob(omicron_path + f'/*/PostProc/unclustered/triggers_unclustered_{ifo}.txt')) 
         trig_file = glob.glob(omicron_path + f'/*/PostProc/unclustered/triggers_unclustered_{ifo}.txt')[0]
         
                           
         # load in triggers 
         triggers = np.loadtxt(trig_file)
-           
+        print(len(triggers)) 
+        # if passed, apply vetos
+        if vetoes is not None:
+            not_vetoed_args = utils.veto(triggers[:,0], vetoes)
+            triggers = triggers[not_vetoed_args]
+        print(len(triggers))
         # restrict triggers to within gps start and stop times
         times = triggers[:,0]
         time_args = np.logical_and(times > start, times < stop)
@@ -64,7 +86,8 @@ def generate_glitch_dataset(
         triggers = triggers[snr_thresh_args] 
     
         snrs.extend(day_snrs) 
-        # crop data for each trigger
+        
+        # query data for each trigger
         for trigger in triggers:
             time = trigger[0]
             trig_data = TimeSeries.fetch_open_data(ifo, time-window, time+window) 
@@ -81,6 +104,8 @@ def main(
     sample_rate: float = 4096,
     outdir: str = './',
     omicron_dir: str ='/home/ethan.marx/bbhnet/generate-glitch-dataset/omicron/',
+    H1_veto_file: str=None,
+    L1_veto_file: str=None
 
 ):
    
@@ -94,8 +119,29 @@ def main(
     - sample_rate: sampling frequency
     - outdir: output directory to which signals will be written
     - omicron_dir: base directory of omicron triggers (see /home/ethan.marx/bbhnet/generate-glitch-dataset/omicron/)
+    - H1_veto_file: path to file containing vetoes for H1
+    - L1_veto_file: path to file containing vetoes for L1
     """ 
     
+    # if passed, load in H1 vetoes and convert to gwpy SegmentList object 
+    if H1_veto_file is not None:
+        
+        # load in H1 vetoes
+        H1_vetoes = np.loadtxt(H1_veto_file)
+        
+        # convert arrays to gwpy Segment objects
+        H1_vetoes = [Segment(seg[0], seg[1]) for seg in H1_vetoes]
+        
+        # create SegmentList object
+        H1_vetoes = SegmentList(H1_vetoes).coalesce()
+    
+    if L1_veto_file is not None:
+    
+        L1_vetoes = np.loadtxt(L1_veto_file)
+        L1_vetoes = [Segment(seg[0], seg[1]) for seg in L1_vetoes]
+        L1_vetoes = SegmentList(L1_vetoes).coalesce()
+    
+
     H1_glitches, H1_snrs = generate_glitch_dataset(
         'H1',
         snr_thresh,
@@ -103,6 +149,7 @@ def main(
         stop,
         window,
         sample_rate,
+        vetoes=H1_vetoes
     )
    
     L1_glitches, L1_snrs = generate_glitch_dataset(
@@ -112,6 +159,7 @@ def main(
         stop,
         window,
         sample_rate,
+        vetoes=L1_vetoes
     )
     
     glitch_file = os.path.join(outdir, 'glitches.h5') 
@@ -127,4 +175,6 @@ def main(
     return glitch_file
 
 if __name__ == "__main__":
-    main(snr_thresh=10, start=1256665622 , stop=1256699999, window=1, sample_rate=4096) 
+    H1_veto_file = '/home/olib/public_html/Ethan/O3b_offline/back_train_lowfreq/vetoes/H1Cat1.txt'
+    L1_veto_file = '/home/olib/public_html/Ethan/O3b_offline/back_train_lowfreq/vetoes/L1Cat1.txt'
+    main(snr_thresh=10, start=1256665622 , stop=1256699999, window=1, sample_rate=4096, H1_veto_file=H1_veto_file, L1_veto_file=L1_veto_file) 
