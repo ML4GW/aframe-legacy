85d3fff445ce8eb8fe6e3467416219ade71903ab
Alec Gunny <alec.gunny@gmail.com>
2022-05-10 08:57:54 -0700

    adding documentation to resnet submodule

--- libs/architectures/bbhnet/architectures/resnet.py
+++ libs/architectures/bbhnet/architectures/resnet.py
@@ -100,6 +100,15 @@ class BasicBlock(nn.Module):
 
 
 class Bottleneck(nn.Module):
+    """
+    Bottleneck blocks implement one extra convolution
+    compared to basic blocks. In this layers, the `planes`
+    parameter is generally meant to _downsize_ the number
+    of feature maps first, which then get expanded out to
+    `planes * Bottleneck.expansion` feature maps at the
+    output of the layer.
+    """
+
     expansion: int = 4
 
     def __init__(
@@ -120,14 +129,22 @@ class Bottleneck(nn.Module):
 
         width = int(planes * (base_width / 64.0)) * groups
 
-        # Both self.conv2 and self.downsample layers
-        # downsample the input when stride != 1
+        # conv1 does no downsampling, just reduces the number of
+        # feature maps from inplanes to width (where width == planes)
+        # if groups == 1 and base_width == 64
         self.conv1 = convN(inplanes, width, kernel_size)
         self.bn1 = norm_layer(width)
+
+        # conv2 keeps the same number of feature maps,
+        # but downsamples along the time axis if stride
+        # or dilation > 1
         self.conv2 = convN(width, width, kernel_size, stride, groups, dilation)
         self.bn2 = norm_layer(width)
+
+        # conv3 expands the feature maps back out to planes * expansion
         self.conv3 = conv1(width, planes * self.expansion)
         self.bn3 = norm_layer(planes * self.expansion)
+
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
         self.stride = stride
@@ -170,7 +187,43 @@ class ResNet(nn.Module):
         layers:
             A list representing the number of residual
             blocks to include in each "layer" of the
-            network.
+            network. Total layers (e.g. 50 in ResNet50)
+            is `2 + sum(layers) * factor`, where factor
+            is `2` for vanilla `ResNet` and `3` for
+            `BottleneckResNet`.
+        kernel_size:
+            The size of the convolutional kernel to
+            use in all residual layers. _NOT_ the size
+            of the input kernel to the network, which
+            is determined at run-time.
+        zero_init_residual:
+            Flag indicating whether to initialize the
+            weights of the batch-norm layer in each block
+            to 0 so that residuals are initialized as
+            identities. Can improve training results.
+        groups:
+            Number of convolutional groups to use in all
+            layers. Grouped convolutions induce local
+            connections between feature maps at subsequent
+            layers rather than global. Generally won't
+            need this to be >1, and wil raise an error if
+            >1 when using vanilla `ResNet`.
+        width_per_group:
+            Base width of each of the feature map groups,
+            which is scaled up by the typical expansion
+            factor at each layer of the network. Meaningless
+            for vanilla `ResNet`.
+        stride_type:
+            Whether to achieve downsampling on the time axis
+            by strided or dilated convolutions for each layer.
+            If left as `None`, strided convolutions will be
+            used at each layer. Otherwise, `stride_type` should
+            be one element shorter than `layers` and indicate either
+            `stride` or `dilation` for each layer after the first.
+        norm_layer:
+            The layer type to use for normalization after each
+            convolution. If left as `None`, defaults to 1D batch norm
+            (`torch.nn.BatchNorm1d`).
     """
 
     block = BasicBlock
@@ -193,6 +246,9 @@ class ResNet(nn.Module):
 
         self.inplanes = 64
         self.dilation = 1
+
+        # TODO: should we support passing a single string
+        # for simplicity here?
         if stride_type is None:
             # each element in the tuple indicates if we should replace
             # the stride with a dilated convolution instead
@@ -205,6 +261,10 @@ class ResNet(nn.Module):
 
         self.groups = groups
         self.base_width = width_per_group
+
+        # start with a basic conv-bn-relu-maxpool block
+        # to reduce the dimensionality before the heavy
+        # lifting starts
         self.conv1 = nn.Conv1d(
             num_ifos,
             self.inplanes,
@@ -217,8 +277,14 @@ class ResNet(nn.Module):
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool1d(kernel_size=3, stride=2, padding=1)
 
+        # now create layers of residual blocks where each
+        # layer uses the same number of feature maps for
+        # all its blocks (some power of 2 times 64).
+        # Don't downsample along the time axis in the first
+        # layer, but downsample in all the rest (either by
+        # striding or dilating depending on the stride_type
+        # argument)
         self.resnet_layers = [self._make_layer(64, layers[0], kernel_size)]
-
         it = zip(layers[1:], stride_type)
         for i, (num_blocks, stride) in enumerate(it):
             block_size = 64 * 2 ** (i + 1)
@@ -231,7 +297,13 @@ class ResNet(nn.Module):
             )
             self.resnet_layers.append(layer)
 
+        # Average pool over each feature map to create a
+        # single value for each feature map that we'll use
+        # in the fully connected head
         self.avgpool = nn.AdaptiveAvgPool1d(1)
+
+        # use a fully connected layer to map from the
+        # feature maps to the binary output that we need
         self.fc = nn.Linear(block_size * self.block.expansion, 1)
 
         for m in self.modules():
@@ -330,5 +402,8 @@ class ResNet(nn.Module):
         return self._forward_impl(x)
 
 
+# TODO: implement as arg of ResNet instead?
 class BottleneckResNet(ResNet):
+    """A version of ResNet that uses bottleneck blocks"""
+
     block = Bottleneck
