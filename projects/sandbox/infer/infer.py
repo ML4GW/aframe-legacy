36aca0033ee2de7383f1ddcc4823feea2de5f37b
Alec Gunny <alec.gunny@gmail.com>
2022-08-24 16:47:56 -0700

    working on last infer bug

--- projects/sandbox/infer/infer.py
+++ projects/sandbox/infer/infer.py
@@ -22,40 +22,67 @@ logging.getLogger("urllib3.connectionpool").setLevel(logging.WARNING)
 
 
 class Sequence:
-    def __init__(self, t, x, write_dir):
+    def __init__(self, t, x, write_dir, batch_size):
         self.t = t
         self.x = x
         self.y = np.zeros_like(t)
-        self.i = 0
+        self.batch_size = batch_size
         self.write_dir = write_dir
 
-    def update(self, y):
-        self.y[self.i : self.i + len(y)] = y[:, 0]
-        self.i += len(y)
+        self._last_seen = -1
+
+    def update(self, y, request_id, sequence_id):
+        if request_id > (self._last_seen + 1):
+            logging.warning(
+                "Dropped response for request {} for sequence {}".format(
+                    self._last_seen + 1, sequence_id
+                )
+            )
+            self._last_seen = request_id
+        elif request_id < self._last_seen:
+            logging.warning(
+                "Request {} from sequence {} came in late".format(
+                    request_id, sequence_id
+                )
+            )
+        else:
+            self._last_seen = request_id
+
+        start = request_id * self.batch_size
+        stop = (request_id + 1) * self.batch_size
+        self.y[start:stop] = y[:, 0]
 
     @property
     def finished(self):
-        return self.i == len(self.t)
+        return self._last_seen == (len(self.x) - 1)
 
 
 class SequenceManager:
-    def __init__(self, executor):
+    def __init__(self, executor, base_sequence_id):
         self.executor = executor
+        self.base_sequence_id = base_sequence_id
+
         self.lock = Lock()
         self.sequences = {}
         self.futures = []
 
-    def add(self, sequence: Sequence, seq_id: int):
-        logging.debug(
+    def add(self, sequence: Sequence):
+        with self.lock:
+            for i in range(len(self.sequences) + 1):
+                seq_id = self.base_sequence_id + i
+                if seq_id not in self.sequences:
+                    break
+            self.sequences[seq_id] = sequence
+
+        logging.info(
             "Managing sequence with t0={} as id {}".format(
                 sequence.t[0], seq_id
             )
         )
-        with self.lock:
-            self.sequences[seq_id] = sequence
+        return seq_id
 
     def __call__(self, y, request_id, sequence_id):
-        self.sequences[sequence_id].update(y)
+        self.sequences[sequence_id].update(y, request_id, sequence_id)
 
         if self.sequences[sequence_id].finished:
             logging.info(f"Finished inference on sequence {sequence_id}")
@@ -90,7 +117,7 @@ def load(
 ):
     hanford, t = segment.load("H1")
     livingston, _ = segment.load("L1")
-    logging.debug(f"Loaded data from segment {segment}")
+    logging.info(f"Loaded data from segment {segment}")
 
     stream_size = stride_size * batch_size
     num_streams = len(t) // stream_size
@@ -100,21 +127,23 @@ def load(
     x = x[:, : num_streams * stream_size].astype("float32")
     x = np.split(x, num_streams, axis=-1)
 
-    sequence = Sequence(t, x, write_dir)
+    sequence = Sequence(t, x, write_dir, batch_size)
     return sequence
 
 
 def infer(
-    sequence: np.ndarray,
+    sequence: Sequence,
     stream_size: int,
-    sequence_id: int,
     manager: SequenceManager,
     client: InferenceClient,
     inference_rate: float,
 ):
-    manager.add(sequence, sequence_id)
+    sequence_id = manager.add(sequence)
     logging.debug(f"Beginning inference on sequence {sequence_id}")
     for i, update in enumerate(sequence.x):
+        logging.debug(
+            "Submitting request {} for sequence {}".format(i, sequence_id)
+        )
         client.infer(
             update,
             request_id=i,
@@ -123,6 +152,13 @@ def infer(
             sequence_end=i == (len(sequence.x) - 1),
         )
         time.sleep(1 / inference_rate)
+
+        # let the first request complete before we
+        # send anymore to make sure that the request
+        # with sequence_start=True gets there first
+        if i == 0:
+            while sequence._last_seen < 0:
+                time.sleep(1e-3)
 
 
 @typeo
@@ -158,13 +194,13 @@ def main(
 
         # submit inference requests using threads so that we
         # can leverage all of our snapshotter states on the server
-        infer_pool = AsyncExecutor(num_workers, thread=True)
+        infer_pool = AsyncExecutor(6, thread=True)
 
         # create a callback which will concatenate the server
         # responses for each parallel sequence we're working
         # on and will submit them to be written once inference
         # has been completed on that sequence
-        callback = SequenceManager(io_pool)
+        callback = SequenceManager(io_pool, base_sequence_id)
 
         # create a client connection to the server and give
         # it the callback to be executed once responses come in
@@ -190,7 +226,7 @@ def main(
                 timeslide = TimeSlide(shift, field)
 
                 futures = []
-                for i, segment in enumerate(timeslide.segments):
+                for segment in timeslide.segments:
                     future = io_pool.submit(
                         load,
                         segment,
@@ -206,7 +242,6 @@ def main(
                             infer,
                             f.result(),
                             stride_size * batch_size,
-                            base_sequence_id + i,
                             callback,
                             client,
                             inference_rate,
