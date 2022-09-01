ebc14773042d00ded77a0bfefb0b0332d99480fd
ethan.marx <ethan.marx@ligo.org>
2022-09-01 10:17:19 -0700

    fix cluster tets

--- libs/analysis/tests/distributions/test_cluster.py
+++ libs/analysis/tests/distributions/test_cluster.py
@@ -3,17 +3,18 @@ import numpy as np
 from bbhnet.analysis.distributions.cluster import ClusterDistribution
 
 
-def test_cluster_distribution(t_clust):
+def test_cluster_distribution():
 
     t_clust = 10
-    distribution = ClusterDistribution("test", ["H", "L", "V"], t_clust)
+    distribution = ClusterDistribution("test", ["H", "L"], t_clust)
 
     t = np.arange(100).astype("float32")
     y = np.random.normal(size=len(t))
 
-    shifts = [0, 1, 1]
+    shifts = [0, 1]
 
     # insert one maximum value every t_clust / 2 seconds
+    # expect these to be the only remaining clustered events
     y[:: int(t_clust / 2)] = 100
 
     distribution.update(y, t, shifts)
