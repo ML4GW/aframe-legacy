7bd7f513f024dabd83b43bb2eaac0b682bb4c354
Alec Gunny <alec.gunny@gmail.com>
2022-09-27 10:44:15 -0700

    getting data tests passing

--- libs/data/tests/test_glitch_sampler.py
+++ libs/data/tests/test_glitch_sampler.py
@@ -6,37 +6,30 @@ import torch
 from bbhnet.data.glitch_sampler import GlitchSampler
 
 
-@pytest.fixture(params=[0, 0.5, 1])
+@pytest.fixture(params=[0, 0.25, 1])
 def prob(request):
     return request.param
 
 
 def get_rand_patch(size):
-    probs = torch.linspace(0, 1, size[1])
+    probs = torch.linspace(0, 0.99, size[1])
     return torch.stack([probs] * size[0])
 
 
-def test_glitch_sampler(
-    arange_glitches,
-    glitch_length,
-    sample_rate,
-    data_length,
-    offset,
-    device,
-    prob,
-):
+def test_glitch_sampler(sample_rate, offset, device, prob):
+    glitches = torch.arange(512 * 2 * 10).reshape(10, 512 * 2) + 1
     sampler = GlitchSampler(
         prob=prob,
         max_offset=offset,
-        h1=arange_glitches[0],
-        l1=arange_glitches[1],
+        h1=glitches,
+        l1=-1 * glitches[:9],
     )
     sampler.to(device)
     for glitch in sampler.glitches:
         assert glitch.device.type == device
 
-    X = torch.zeros((8, 2, 512))
-    probs = get_rand_patch((2, 8))
+    X = torch.zeros((8, 2, 512)).to(device)
+    probs = get_rand_patch((2, 8)).to(device)
     with patch("torch.rand", return_value=probs):
         inserted, _ = sampler(X, None)
 
@@ -47,5 +40,5 @@ def test_glitch_sampler(
         elif prob == 1:
             assert (inserted != 0).all().item()
         else:
-            assert (inserted[:4] == 0).all().item()
-            assert (inserted[4:] != 0).all().item()
+            assert (inserted[:2] != 0).all().item()
+            assert (inserted[2:] == 0).all().item()
