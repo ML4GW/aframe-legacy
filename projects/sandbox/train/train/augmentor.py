b93970de4c37d675240d6a36e761f4b603a13f80
Ethan Jacob Marx <ethan.marx@ligo.org>
2023-07-20 08:41:26 -0700

    update augmentor for glitch loading

--- projects/sandbox/train/train/augmentor.py
+++ projects/sandbox/train/train/augmentor.py
@@ -22,6 +22,7 @@ class AframeBatchAugmentor(torch.nn.Module):
         ifos: List[str],
         sample_rate: float,
         signal_prob: float,
+        glitch_downweight: float,
         dec: Callable,
         psi: Callable,
         phi: Callable,
@@ -41,6 +42,7 @@ class AframeBatchAugmentor(torch.nn.Module):
         signal_prob = signal_prob / (
             1 - (swap_frac + mute_frac - (swap_frac * mute_frac))
         )
+        self.glitch_downweight = glitch_downweight
 
         if not 0 < signal_prob <= 1.0:
             raise ValueError(
@@ -127,16 +129,57 @@ class AframeBatchAugmentor(torch.nn.Module):
         )
         return kernels
 
+    def insert_glitches(self, X, glitches, y):
+        glitches = glitches.transpose(1, 0)
+        # loop over channels and insert glitches
+        for i, tensor in enumerate(glitches):
+
+            # randomly sample batch indices which
+            # will be replaced with a glitch
+            idx = torch.randperm(len(X))[: glitches.shape[1]]
+
+            # sample kernels from the selected glitches.
+            # Add a dummy dimension so that sample_kernels
+            # doesn't think this is a single multi-channel
+            # timeseries, but rather a batch of single
+            # channel timeseries
+            tensor = tensor[None]
+            tensor = sample_kernels(
+                tensor,
+                kernel_size=X.shape[-1],
+            )
+
+            # replace the appropriate channel in our
+            # strain data with the sampled glitches
+            X[idx, i] = tensor[:, 0]
+
+            # use bash file permissions style
+            # numbers to indicate which channels
+            # go inserted on
+
+            y[idx] -= 2 ** (i + 1)
+
+        return X, y
+
     @torch.no_grad()
-    def forward(self, X, y):
+    def forward(self, X, glitches, y):
+        # first insert glitches into the batch
+        X, y = self.insert_glitches(X, glitches, y)
+
+        # estimate PSDs
         X, psds = self.psd_estimator(X)
 
-        # apply inversion / flip augementations
+        # apply inversion / flip augmentations
         X = self.inverter(X)
         X = self.reverser(X)
 
         # calculate number of waveforms to generate
+        # based on waveform prob, mute prob, and swap prob and downweight
+        # likelihood of injecting a signal on top of a glitch.
+        # y == -2 means one glitch, y == -6 means two
         probs = torch.ones_like(y) * self.signal_prob
+        probs[y < 0] *= self.glitch_downweight
+        probs[y < -4] *= self.glitch_downweight
         rvs = torch.rand(size=X.shape[:1], device=probs.device)
         mask = rvs < probs[:, 0]
 
@@ -151,6 +194,12 @@ class AframeBatchAugmentor(torch.nn.Module):
         responses, swap_indices = self.swapper(responses)
         responses, mute_indices = self.muter(responses)
         X[mask] += responses
+
+        # now that glitches have been inserted and
+        # injections have been made,
+        # whiten _all_ the strain using the
+        # background psds computed up top
+        X = self.whitener(X, psds)
 
         # now that injections have been made,
         # whiten _all_ the strain using the
