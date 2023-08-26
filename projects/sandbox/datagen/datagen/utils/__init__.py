b66e41810b0ee0930a20c3b3f86063fc0f2f9690
Ethan Marx <emarx@mit.edu>
2023-08-25 19:18:13 -0500

    use 'DATA' as value for open data segments

--- projects/sandbox/datagen/datagen/utils/__init__.py
+++ projects/sandbox/datagen/datagen/utils/__init__.py
@@ -1,8 +1,8 @@
 from typing import List, Optional
 
 
-def get_state_flags(ifos: List[str], state_flag: Optional[str] = None):
-    if state_flag is None:
+def get_state_flags(ifos: List[str], state_flag: str):
+    if state_flag == "DATA":
         flags = [f"{ifo}_DATA" for ifo in ifos]  # open data flags
     else:
         flags = [f"{ifo}:{state_flag}" for ifo in ifos]
