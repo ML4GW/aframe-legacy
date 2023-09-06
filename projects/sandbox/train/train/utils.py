ff6038833a941f01ab4ec310bdfb390ec533b998
Alec Gunny <alec.gunny@gmail.com>
2023-09-06 11:05:20 -0700

    adding seeding during training and switching to median

--- projects/sandbox/train/train/utils.py
+++ projects/sandbox/train/train/utils.py
@@ -21,6 +21,12 @@ def seed_worker(worker_id):
     np.random.seed(worker_seed)
     random.seed(worker_seed)
 
+def seed_worker(worker_id):
+    worker_seed = torch.initial_seed() % 2**32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
+
+
 def split(X: Tensor, frac: float, axis: int) -> Tuple[Tensor, Tensor]:
     """
     Split an array into two parts along the given axis
