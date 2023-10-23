faaaf512adb95a667c9706f1f71aa439bb6d5278
Rafia Omer <omerx054@umn.edu>
2023-10-23 13:46:51 -0500

    fix for N=0 and proper psd length

--- projects/sandbox/train/train/train.py
+++ projects/sandbox/train/train/train.py
@@ -228,11 +228,10 @@ def main(
     sample_length = window_length + psd_length
     fftlength = fftlength or window_length
 
-
     if psd_length < window_length:
         raise ValueError(
-	     "Can't have psd length {} longer than "
-	     "window length {}".format(psd_length, window_length)
+	     "Can't have psd length shorter than "
+	     "window length, {} < {}".format(psd_length, window_length)
         )
 
     # create objects that we'll use for whitening the data
